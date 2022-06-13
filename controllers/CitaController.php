f71b8fffdf130501547dc1f69ee606bddfe59eef
emmanuelcc3 <75090995+emmanuelcc3@users.noreply.github.com>
2022-06-12 22:07:19 -0600

    AppSalon
    Signed-off-by: emmanuelcc3 <75090995+emmanuelcc3@users.noreply.github.com>

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -9,7 +9,7 @@ class CitaController{
 
     public static function index(Router $router){
 
-        session_start();
+        isSession();
 
         $router->render('cita/index', [
             'nombre' => $_SESSION['nombre']
--- views/cita/index.php
+++ views/cita/index.php
@@ -3,6 +3,11 @@
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
 <div id="app">
+    <nav class = "tabs">
+        <button class="actual" type="button" data-paso="1">Servicios</button>
+        <button type="button" data-paso="2">Informacion Cita</button>
+        <button type="button" data-paso="3">Resumen</button>
+    </nav>
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
         <p class="text-center">Elige tus servicios a continuacion</p>
@@ -41,5 +46,10 @@
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
         <p class="text-center">Verifica que la informacion sea correcta</p>
+    </div>
+
+    <div class="paginacion">
+        <button class="boton" id="anterior">&laquo; Anterior</button>
+        <button class="boton" id="siguiente">Siguiente &raquo;</button>
     </div>
 </div>
