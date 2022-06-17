71d7fd7d3da042928ae6e0a03954745636ba6875
emmanuelcc3 <75090995+emmanuelcc3@users.noreply.github.com>
2022-06-17 11:12:11 -0600

    Actualizado

--- views/cita/index.php
+++ views/cita/index.php
@@ -17,40 +17,43 @@
         <h2>Tus Datos y Cita</h2>
         <p class="text-center">Coloca tus datos y fecha de tu cita</p>
 
-        <form class="formulario"></form>
-         <div class="campo">
-          <label for="nombre">Nombre:</label>
-          <input
-              id="nombre"
-              type="text"
-              placeholder="Tu Nombre"
-              value="<?php echo $nombre; ?>"
-              disabled
-          />
-         </div>
-         <div class="campo">
-          <label for="fecha">Fecha:</label>
-          <input
-              id="fecha"
-              type="date"
-          />
-         </div>
-         <div class="campo">
-          <label for="hora">Hora:</label>
-          <input
-              id="hora"
-              type="time"
-          />
-         </div>
-    </div>
-    <div id="paso-3" class="seccion">
-        <h2>Resumen</h2>
-        <p class="text-center">Verifica que la informacion sea correcta</p>
-    </div>
+        <form class="formulario">
 
-    <div class="paginacion">
-        <button  id="anterior" class="boton">&laquo; Anterior</button>
-        <button id="siguiente" class="boton" >Siguiente &raquo;</button>
+            <div class="campo">
+                <label for="nombre">Nombre:</label>
+                <input
+                id="nombre"
+                type="text"
+                placeholder="Tu Nombre"
+                value="<?php echo $nombre; ?>"
+                disabled
+                />
+            </div>
+            <div class="campo">
+                <label for="fecha">Fecha:</label>
+                <input
+                id="fecha"
+                type="date"
+                min="<?php echo date('Y-m-d', strtotime('+1 day') ); ?>"
+                />
+            </div>
+            <div class="campo">
+                <label for="hora">Hora:</label>
+                <input
+                id="hora"
+                type="time"
+                />
+            </div>
+        </form>
+        </div>
+        <div id="paso-3" class="seccion contenido-resumen">
+            <h2>Resumen</h2>
+            <p class="text-center">Verifica que la informacion sea correcta</p>
+        </div>
+        
+        <div class="paginacion">
+            <button  id="anterior" class="boton">&laquo; Anterior</button>
+            <button id="siguiente" class="boton" >Siguiente &raquo;</button>
     </div>
 </div>
 
