59a2b3953a247bd2eb23d756513c2f7b5edfba93
emmanuelcc3 <75090995+emmanuelcc3@users.noreply.github.com>
2022-07-06 13:58:39 -0600

    Actualizacion

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -2,14 +2,33 @@
 
 namespace Controllers;
 
+use Model\AdminCita;
 use MVC\Router;
 
 class AdminController {
 
     public static function index(Router $router) {
         isSession();
+
+        //Consultar la base de datos de la base
+        $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
+$consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
+$consulta .= " FROM citas  ";
+$consulta .= " LEFT OUTER JOIN usuarios ";
+$consulta .= " ON citas.usuarioId=usuarios.id  ";
+$consulta .= " LEFT OUTER JOIN citasservicio ";
+$consulta .= " ON citasservicio.citaId=citas.id ";
+$consulta .= " LEFT OUTER JOIN servicios ";
+$consulta .= " ON servicios.id=citasservicio.servicioId ";
+// $consulta .= " WHERE fecha =  '${fecha}' ";
+
+$citas = AdminCita::SQL($consulta);
+
+
+
         $router->render('admin/index',[
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'citas'  => $citas
         ]);
 
     }
--- views/admin/index.php
+++ views/admin/index.php
@@ -1,4 +1,41 @@
-<h1 class ="nombre-pagina">Panel de Administrator</h1>
+<h1 class="nombre-pagina">Panel de Administrator</h1>
 
 
-<?php include_once __DIR__ . '/../templates/barra.php'?>
+<?php include_once __DIR__ . '/../templates/barra.php' ?>
+
+<h2>Buscar cita</h2>
+<div class="busqueda">
+    <form class="formulario">
+        <div class="campo">
+            <label class="fecha">Fecha</label>
+            <input type="date" id="fecha" name="fecha" />
+
+    </form>
+</div>
+
+<div id="citas-admin">
+    <ul class="citas">
+        <?php
+        $idCita = 0;
+        foreach ($citas as $cita) {
+            if ($idCita !== $cita->id) {
+        ?>
+                <li>
+                    <p>ID: <span><?php echo $cita->id; ?></span></p>
+                    <p>Hora: <span><?php echo $cita->hora; ?></span></p>
+                    <p>Nombre: <span><?php echo $cita->cliente; ?></span></p>
+                    <p>Email: <span><?php echo $cita->email; ?></span></p>
+                    <p>Teléfono: <span><?php echo $cita->telefono; ?></span></p>
+
+                    <h3>Servicios</h3>
+
+                <?php
+                $idCita = $cita->id;
+            } //Fin de IF 
+                ?>
+                <p class="servicio"><?php echo $cita->servicio . " " . "$" . $cita->precio; ?></p>
+                </li>
+            <?php } //Fin de Foreach 
+            ?>
+    </ul>
+</div>
